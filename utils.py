1f5ef35e18b0fd64b7ce2a2cbd9e02ccc3c45b7a
Thien K. M. Bui <kevinbui904@gmail.com>
2023-09-19 10:00:23 -0500

    changed states to contain dictionary of key and array values

--- utils.py
+++ utils.py
@@ -9,17 +9,21 @@ def my_list_function(x):
     return my_list
 
 class CompartmentalModel:
-    def __init__(self, starting_value, compartments = None):
+    def __init__(self, start_states = None, compartments = None):
         """compartments is a list of functions that each take in 
         the current state and current time step and return how 
         much the state changes due to that compartment."""
 
-        self.starting_value = starting_value
+        # self.starting_value = starting
         self.compartments = compartments
 
         # Eventually store all of the states in a list, so that if we 
         # later want to compute more, we don't have to re-compute the early states
-        self.states = [starting_value]
+        
+        #This will containing a dictionary with keys and array values
+        #{deer: [100, 200, 100]}, plant:[3000, 2000, 1000]
+        self.states = start_states
+        
         self.max_timestep_computed = 0
 
     def get_states(self):
@@ -32,6 +36,7 @@ class CompartmentalModel:
             # Compute the new state
             new_state = self.states[-1]
             # Use each compartment function to get a new piece of the state
+            # Compute a new state for each time_step, update only at the end
             for compartment in self.compartments:
                 cur_state = new_state
                 new_state += compartment(cur_state, i)
